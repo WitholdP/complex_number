d32c01eaa2945b7e453a0dba4f798c49206afbe5
Witold Piecewicz <w.piecewicz@gmail.com>
2024-10-29 00:14:47 +0100

    overloaded division operator

--- main.cpp
+++ main.cpp
@@ -47,8 +47,20 @@ public:
         return multResult;
     }
 
-    int m_a;
-    int m_b;
+    ComplexNumber operator/(ComplexNumber &obj)
+    {
+        if (obj.m_a == 0 && obj.m_b == 0)
+        {
+            throw invalid_argument("You cannot divide by zero");
+        }
+
+        ComplexNumber divResult;
+        int divider = (obj.m_a * obj.m_a + obj.m_b * obj.m_b);
+        divResult.m_a = (m_a * obj.m_a + m_b * obj.m_b) / divider;
+        divResult.m_b = (m_b * obj.m_a - m_a * obj.m_b) / divider;
+
+        return divResult;
+    }
 
     void Print()
     {
@@ -69,16 +81,29 @@ public:
             cout << m_a << m_b << "i" << endl;
         }
     }
+
+    float m_a;
+    float m_b;
 };
 
 int main()
 {
-    ComplexNumber *complexNumber1 = new ComplexNumber(1, 1);
-    ComplexNumber *complexNumber2 = new ComplexNumber(2, 2);
+    ComplexNumber *complexNumber1 = new ComplexNumber(10, 10);
+    ComplexNumber *complexNumber2 = new ComplexNumber(7, 7);
     ComplexNumber *complexNumberCopy = new ComplexNumber(*complexNumber1);
     ComplexNumber complexNumberAdd = *complexNumber1 + *complexNumber2;
     ComplexNumber complexNumberSubt = *complexNumber1 - *complexNumber2;
     ComplexNumber complexNumberMult = *complexNumber1 * *complexNumber2;
+
+    try
+    {
+        ComplexNumber complexNumberDiv = *complexNumber1 / *complexNumber2;
+        complexNumberDiv.Print();
+    }
+    catch (const std::invalid_argument &e)
+    {
+        cout << e.what() << endl;
+    }
 
     complexNumberCopy->Print();
     complexNumber1->Print();
